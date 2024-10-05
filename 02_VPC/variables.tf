cc31daa03cc6d1da647ce90d611ec2e53dcdbcd8
Stone Kim <Mon8Cats@gmail.com>
2024-10-05 12:28:09 -0500

    add variables

---
+++ 02_VPC/variables.tf
@@ -0,0 +1,11 @@
+# variables.tf
+
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region_name" {
+  description = "The GCP region name"
+  type        = string
+}
